b8ab4a42a5a39f694b68684e6e673f0b43af137f
Scriptinfano <2996296637@qq.com>
2022-09-16 22:51:12 +0800

    无

--- src/testEverything/TestMain.java
+++ src/testEverything/TestMain.java
@@ -3,23 +3,35 @@
  */
 package testEverything;
 
-class Person {
-    int age;
-    String name;
-
-    public Person(int age, String name) {
-        this.age = age;
-        this.name = name;
-    }
-
-}
+import java.util.Objects;
+import java.util.concurrent.ArrayBlockingQueue;
 
 public class TestMain {
 
     public static void main(String[] args) {
 
-        Person p = new Person(12, "wang");
 
+        ArrayBlockingQueue<Integer> queue = new ArrayBlockingQueue<>(10);
+        queue.offer(12);
+        queue.offer(13);
+        queue.offer(14);
+        queue.offer(15);
+        queue.offer(12);
+        queue.offer(13);
+        queue.offer(14);
+        queue.offer(15);
+        System.out.println(queue);
+        boolean you = YouXv(queue);
+        System.out.println(you);
 
+    }
+
+    public static boolean YouXv(ArrayBlockingQueue<Integer> queue) {
+        while (!queue.isEmpty()) {
+            Integer m1 = queue.poll();
+            Integer m2 = queue.peek();
+            if (m1.compareTo(Objects.requireNonNull(m2)) > 0) return false;
+        }
+        return true;
     }
 }
