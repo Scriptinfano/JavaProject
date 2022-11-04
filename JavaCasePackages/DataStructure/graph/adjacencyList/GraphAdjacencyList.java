9e471029c3f5b6badc9644a16f9555e0f04f7421
Scriptinfano <2996296637@qq.com>
2022-11-04 11:35:09 +0800

    无

--- JavaCasePackages/DataStructure/graph/adjacencyList/GraphAdjacencyList.java
+++ JavaCasePackages/DataStructure/graph/adjacencyList/GraphAdjacencyList.java
@@ -131,64 +131,85 @@ class HeadNode {
 
 
 /**
- * 迪杰斯特拉算法求解器，内含该算法所需的三个数组
+ * 迪杰斯特拉算法求解器，内含该算法所需的三个数组，使用方法是传入存储所有顶点的容器和该算法所需的起始点然后调用接口run()
  *
  * @author Mingxiang
  */
 class DijkstraSolver {
+    /**
+     * 标记顶点是否已被访问
+     */
     private final boolean[] hasVisited;
+    /**
+     * 起始点到各顶点的最短距离
+     */
     private final Integer[] distance;
-    private final HeadNode[] parent;
-    private final HeadNode startNode;
-    private final HeadNode endNode;
-    private ArrayList<HeadNode> pathCollection;//所有进入该集合的顶点，都表示该顶点已经算在最短路径中了
-
-    private int theTotalDistance;
 
     /**
-     * dijkstra算法解算器的构造函数
-     *
-     * @param size 该算法所要求解的图中有几个顶点
+     * 各顶点的上一个访问的节点
      */
-    public DijkstraSolver(int size, HeadNode theStartNode, HeadNode theEndNode) {
+    private final HeadNode[] parent;
+
+    /**
+     * Dijkstra算法的起始点
+     */
+    private final HeadNode startNode;
+
+    /**
+     * 存储所有顶点的容器
+     */
+    private final ArrayList<HeadNode> headNodeList;
+
+    /**
+     * 所有进入该集合的顶点，都表示该顶点已经算在最短路径中了
+     */
+    private final ArrayList<HeadNode> pathCollection;
+
+
+    /**
+     * dijkstra算法解算器构造器
+     *
+     * @param theHeadNodeList 存储图中所有顶点的容器
+     * @param theStartNode    Dijkstra算法求解的是一个顶点到图中所有顶点的最短路径，所以需要一个起始顶点作为遍历的起始点
+     */
+    public DijkstraSolver(ArrayList<HeadNode> theHeadNodeList, HeadNode theStartNode) {
         pathCollection = new ArrayList<>();
+        int size = theHeadNodeList.size();
         hasVisited = new boolean[size];//java的boolean数组默认初始化时，所有元素均为false
         distance = new Integer[size];
         parent = new HeadNode[size];
         startNode = theStartNode;
-        endNode = theEndNode;
-        theTotalDistance = 0;
+        headNodeList = theHeadNodeList;
 
-        //更新起点的信息
-        update(startNode, 0, null);
-        //将起点纳入路径
-        pathCollection.add(startNode);
-        hasVisited[startNode.getIndex()] = true;
+        if (!theHeadNodeList.isEmpty()) {
+            //更新起点的信息
+            update(headNodeList.get(0), 0, null);
+            pathCollection.add(headNodeList.get(0));
+            hasVisited[headNodeList.get(0).getIndex()] = true;
+        } else throw new RuntimeException("构造DijkstraSolver时传入的ArrayList<HeadNode>容器为空，无法初始化");
 
     }
 
-    public void run(ArrayList<HeadNode> headNodeList) {
-        //FIXME 该算法仍然有bug，待修改
+    public void run() {
         HeadNode currentNode = startNode;
+        //从startNode开始依次遍历所有顶点
 
-        while (currentNode != endNode) {
-
+        //while循环的条件是当pathCollection中包含所有顶点时结束循环
+        while (!pathCollection.containsAll(headNodeList)) {
             //依次检索所有不在最短路径集合中的且与当前节点相连的顶点，并更新这些节点在三个数组中的信息
             for (int i = 0; i < currentNode.getOutDegree(); i++) {
                 HeadNode theAdjacencyNode = currentNode.getAdjacencyNode(i);//当前正在处理的邻接点
                 //检查该邻接点是否已被纳入最短路径集合
                 if (!pathCollection.contains(theAdjacencyNode)) {
                     int newDistance = distance[currentNode.getIndex()] + currentNode.getLineDistanceByOrder(i);//到达该顶点的最短距离加上目前正在遍历的邻接点到该顶点的距离
-                    int oldDistance = distance[theAdjacencyNode.getIndex()];
-                    if (distance[theAdjacencyNode.getIndex()] == null || newDistance < oldDistance)
+                    if (distance[theAdjacencyNode.getIndex()] == null || newDistance < distance[theAdjacencyNode.getIndex()])
                         update(theAdjacencyNode, newDistance, currentNode);
-                    else update(theAdjacencyNode, oldDistance, currentNode);
+                    else update(theAdjacencyNode, distance[theAdjacencyNode.getIndex()], currentNode);
                 }
             }
             int minIndex = scan();//未选顶点中的distance值最小的顶点的编号
             hasVisited[minIndex] = true;//minIndex所对应的顶点是即将添加到最短路径的顶点，所以将hasVisited的对应位置置true
 
-            //TODO 如何计算路径的总长度，由于现在这个算法算出的最短路径有分叉且有点小问题（到七号节点时应该直接走八号节点，但目前走了六号节点然后才走八号节点）
             currentNode = headNodeList.get(minIndex);//更新最短路径中的最新节点
             pathCollection.add(currentNode);//将最新的节点加入已选择的节点集合中
 
@@ -215,7 +236,7 @@ class DijkstraSolver {
         }
         List<Map.Entry<Integer, Integer>> list = new ArrayList<>(sorter.entrySet());
 
-        list.sort(new Comparator<Map.Entry<Integer, Integer>>() {
+        list.sort(new Comparator<>() {
             @Override
             public int compare(Map.Entry<Integer, Integer> o1, Map.Entry<Integer, Integer> o2) {
                 return Integer.compare(o1.getValue() - o2.getValue(), 0);
@@ -227,13 +248,40 @@ class DijkstraSolver {
     }
 
     /**
-     * 返回最短路径集合
+     * 得到起始顶点到指定顶点的最短路径
      *
-     * @return {@link ArrayList}<{@link HeadNode}> 最短路径集合，其中HeadNode的顺序就是最短路径
+     * @param index 指定顶点在图中的编号
+     * @return int 最短路径值
      */
-    public ArrayList<HeadNode> getPathCollection() {
-        return pathCollection;
+    public int getShortestPathLength(int index) {
+        if (index < 0 || index >= distance.length) {
+            throw new IllegalArgumentException("得到起始顶点到指定顶点的最短路径时指定的顶点编号不存在");
+        }
+        return distance[index];
     }
+
+    /**
+     * 得到起始顶点到指定顶点的路径集合，其中包含起始顶点到指定顶点的所有路径节点
+     *
+     * @param index 指定的顶点在图中的编号
+     * @return {@link ArrayList}<{@link HeadNode}>
+     */
+    public ArrayList<HeadNode> getShortestPath(int index) {
+        if (index < 0 || index >= distance.length) {
+            throw new IllegalArgumentException("得到起始顶点到指定顶点的最短路径序列时指定的顶点编号不存在");
+        }
+        ArrayList<HeadNode> shortestPathCollection = new ArrayList<HeadNode>();
+        shortestPathCollection.add(headNodeList.get(index));
+        HeadNode currentParentNode = parent[index];
+        shortestPathCollection.add(currentParentNode);
+        while (currentParentNode != startNode) {
+            currentParentNode = parent[currentParentNode.getIndex()];
+            shortestPathCollection.add(currentParentNode);
+        }
+        Collections.reverse(shortestPathCollection);
+        return shortestPathCollection;
+    }
+
 }
 
 /**
@@ -435,17 +483,20 @@ public class GraphAdjacencyList {
     }
 
     /**
-     * 使用迪杰斯特拉算法寻找最短路径
+     * 使用迪杰斯特拉算法求得指定的起始顶点到指定的顶点的最短路径集合
      *
-     * @param theStartNode 路径的起始节点
-     * @param theEndNode   路径的结束节点
+     * @param startIndex  路径的起始节点的编号
+     * @param targetIndex 指定的顶点的编号
      * @return {@link ArrayList}<{@link HeadNode}> 返回一个由有序顶点组成的ArrayList容器，顶点的顺序就是最短的路径
      */
-    public ArrayList<HeadNode> getShortestPathByDijkstra(HeadNode theStartNode, HeadNode theEndNode) {
-        DijkstraSolver dijkstraSolver = new DijkstraSolver(graphList.size(), theStartNode, theEndNode);
-        dijkstraSolver.run(graphList);
-        return dijkstraSolver.getPathCollection();
+    public ArrayList<HeadNode> getShortestPathCollectionByDijkstra(int startIndex, int targetIndex) {
+        if (startIndex < 0 || targetIndex < 0 || startIndex >= graphList.size() || targetIndex >= graphList.size())
+            throw new IllegalArgumentException("调用getShortestPathByDijkstra()函数时传入的起始顶点编号和终止顶点编号不符合要求");
+        DijkstraSolver solver = new DijkstraSolver(graphList, graphList.get(startIndex));
+        solver.run();//运行解算器
+        return solver.getShortestPath(targetIndex);
     }
+
 
     /**
      * 图的一次广度优先遍历，仅遍历一个连通分量
@@ -514,7 +565,7 @@ class TestGraphList {
 
         printer.println();
 
-        ArrayList<HeadNode> pathList = graph.getShortestPathByDijkstra(graph.getHeadNode(0), graph.getHeadNode(8));
+        ArrayList<HeadNode> pathList = graph.getShortestPathCollectionByDijkstra(0, 8);
         for (HeadNode theNode : pathList) {
             printer.print(theNode.getIndex() + " ");
         }
