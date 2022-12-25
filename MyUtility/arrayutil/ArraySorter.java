3f21053d107960f2f4f66dc23a59feb74473fa25
Scriptinfano <2996296637@qq.com>
2022-12-25 17:21:42 +0800

    无

--- MyUtility/arrayutil/ArraySorter.java
+++ MyUtility/arrayutil/ArraySorter.java
@@ -11,7 +11,7 @@ public class ArraySorter<T extends Comparable<T>> {
     /**
      * 内部要排列的数组
      */
-    private T[] array = null;
+    private T[] array;
 
     /**
      * 构造ArraySorter对象，将要进行排序的数组作为属性传入ArraySorter对象，以便其调用排序算法对其进行排序
@@ -27,14 +27,6 @@ public class ArraySorter<T extends Comparable<T>> {
      */
     public void resetArray(T[] array) {
         this.array = array;
-    }
-
-
-    public static void main(String[] args) {
-        Integer[] arr = ArrayUtil.randomIntegerArray(10, 1, 100);
-        ArrayUtil.showArray(arr);
-        ArraySorter<Integer> sorter = new ArraySorter<>(arr);
-        sorter.binaryInsertSort();
     }
 
     /**
@@ -107,6 +99,14 @@ public class ArraySorter<T extends Comparable<T>> {
     }
     */
 
+    public static void main(String[] args) {
+        Integer[] arr = ArrayUtil.randomIntegerArray(100, 1, 1000);
+        System.out.println(Arrays.toString(arr));
+        ArraySorter<Integer> sorter = new ArraySorter<>(arr);
+        sorter.simpleSelectionSort();
+        System.out.println(Arrays.toString(arr));
+    }
+
     /**
      * 直接插入排序<p>
      * 插入排序的思想:从数组的第二个元素开始遍历数组，比较该元素与前一个元素，如果该元素
@@ -114,41 +114,15 @@ public class ArraySorter<T extends Comparable<T>> {
      * 将这个元素不断地向后挪，直到该元素比后一个元素小为止，挪的过程中采用将前一个元素覆
      * 写后一个元素的方式实现，不用担心后一个元素被抹掉，因为已经提前保存了这个值
      * <p>
-     *     算法特点：<p>
-     *     1、稳定排序<p>
-     *     2、适合链式存储结构<p>
-     *     3、适合初始基本有序且数组元素个数较小的情况<p>
+     * 算法特点：<p>
+     * 1、稳定排序<p>
+     * 2、适合链式存储结构<p>
+     * 3、适合初始基本有序且数组元素个数较小的情况<p>
      */
     public void insertSort() {
         for (int i = 1; i < array.length; i++) {
             T value = array[i];
             insert(i, value);
-        }
-    }
-
-    /**
-     * 折半插入排序
-     * <p>
-     *     算法特点：
-     *     <p>
-     *        1、排序稳定<p>
-     *        2、只能用于顺序结构<p>
-     *        3、适合初始记录无序，数组元素数较大的情况<p>
-     */
-    public void binaryInsertSort() {
-
-        for (int i = 1; i < array.length; i++) {
-            T temp = array[i];
-            int low = 0, high = i - 1;
-            while (low <= high) {
-                int mid = (low + high) / 2;
-                if (temp.compareTo(array[mid]) < 0)
-                    high = mid - 1;
-                else low = mid + 1;
-            }
-            for (int j = i - 1; j >= high + 1; j--)
-                array[j + 1] = array[j];
-            array[high + 1] = temp;
         }
     }
 
@@ -220,6 +194,32 @@ public class ArraySorter<T extends Comparable<T>> {
     }
 
     /**
+     * 折半插入排序
+     * <p>
+     * 算法特点：
+     * <p>
+     * 1、排序稳定<p>
+     * 2、只能用于顺序结构<p>
+     * 3、适合初始记录无序，数组元素数较大的情况<p>
+     */
+    public void binaryInsertSort() {
+
+        for (int i = 1; i < array.length; i++) {
+            T temp = array[i];
+            int low = 0, high = i - 1;
+            while (low <= high) {
+                int mid = (low + high) / 2;
+                if (temp.compareTo(array[mid]) < 0)
+                    high = mid - 1;
+                else low = mid + 1;
+            }
+            for (int j = i - 1; j >= high + 1; j--)
+                array[j + 1] = array[j];
+            array[high + 1] = temp;
+        }
+    }
+
+    /**
      * 名次排序<p>
      * 数组中元素的名次是什么：一个元素在一个序列中的名次是所有比它小的元素个数加上
      * 在它左边出现的与它相同的元素个数
@@ -237,7 +237,7 @@ public class ArraySorter<T extends Comparable<T>> {
                 else rankArray[j]++;
             }
         }
-        ArraySorter sorter = new ArraySorter(rankArray);
+        ArraySorter<Integer> sorter = new ArraySorter<>(rankArray);
 
         for (int i = 0; i < array.length; i++) {
             while (!rankArray[i].equals(i)) {
@@ -249,22 +249,20 @@ public class ArraySorter<T extends Comparable<T>> {
 
     /**
      * 简单选择排序
+     * <p>算法特点<p>
+     * 1、不稳定排序<p>
+     * 2、可用于链式存储结构<p>
+     * 3、适用于移动记录次数较少，每一记录占用空间较多时的情况
      */
     public void simpleSelectionSort() {
-
-    }
-
-    /**
-     * 在java中交换数组中的两个元素<p>
-     * 注意在java中交换数组的两个元素必须给函数传入数组引用以及两个要交换的元素的下标
-     *
-     * @param i 要交换的第一个元素的下标
-     * @param j 要交换的第二个元素的下标
-     */
-    private void swap(int i, int j) {
-        T temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
+        for (int i = 0; i < array.length - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < array.length; j++)
+                if (array[j].compareTo(array[min]) < 0)
+                    min = j;
+            if (min != i)
+                swap(i, min);
+        }
     }
 
     /**
@@ -284,5 +282,46 @@ public class ArraySorter<T extends Comparable<T>> {
         }
     }
 
+    /**
+     * 在排序的诸多算法中涉及交换数组中的两个元素，故有此交换函数<p>
+     * @param i 要交换的第一个元素的下标
+     * @param j 要交换的第二个元素的下标
+     */
+    private void swap(int i, int j) {
+        T temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
 
+    /**
+     * 堆排序
+     */
+    public void heapSort() {
+        for (int k = array.length / 2; k >= 1; k--)
+            biggerHeapAdjust(k, array.length - 1);
+        for (int k = 0; k < array.length; k++) {
+            swap(0, array.length - k + 1);
+        }
+    }
+
+    /**
+     * 大根堆调整函数，将整个无序数组视为完全二叉树后，对其进行调整，使其符合大根堆的定义
+     *
+     * @param begin 待调整节点在整个堆中按照层序遍历的编号
+     * @param end   整个堆最后一个节点的层序遍历编号
+     */
+    private void biggerHeapAdjust(int begin, int end) {
+        int i = begin, j = 2 * i;
+        while (j <= end) {
+
+            //j<end实际是在判断i所指节点有没有右子树，如果有右子树，则必然满足此条件，则说明有右子树
+            if (j < end && array[j].compareTo(array[j + 1]) < 0)
+                j++;
+            if (array[i].compareTo(array[j]) < 0)
+                swap(i, j);
+            //如果此时子树又不符合堆的定义，那么更新i,j的值去调整子树
+            i = j;
+            j = 2 * i;
+        }
+    }
 }
