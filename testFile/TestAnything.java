1cea621102de5fef03722ebfe91ff3eb8653c79a
Mingxiang <2996296637@qq.com>
2023-01-10 23:35:51 +0800

    无

--- testFile/TestAnything.java
+++ testFile/TestAnything.java
@@ -1,3 +1,4 @@
+
 class TestMain2 {
     public static void main(String[] args) {
         /*ArrayList<Person> persons=new ArrayList<>();
@@ -11,6 +12,8 @@ class TestMain2 {
             person.output();
         }*/
         System.out.format("\33[31;1m*\33[0m%n");
-
+        double x = 2, y = 3;
+        double result=Math.sqrt(Math.pow(x, 2)+ Math.pow(y, 2)) ;
+        System.out.println(result);
     }
 }
